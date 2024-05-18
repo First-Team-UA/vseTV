c657827a51a76ab3b2cf06ce34d50e64ca68f426
Mykyta-Ushakow <nilitosushak@gmail.com>
2024-05-18 16:39:33 +0300

    Database is connected

--- backend/src/services/database.ts
+++ backend/src/services/database.ts
@@ -1,23 +1,30 @@
+import fs from "fs"
 import dotenv from "dotenv"
 import mysql from "mysql2/promise"
+
+dotenv.config({ path: "../.env" })
+
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+})
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    ca: fs.readFileSync("../ca-certificate.crt"),
+    rejectUnauthorized: true,
+  },
 })
 
-dotenv.config()
-
 async function testConnection() {
-  const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  })
-
   try {
     const connection = await pool.getConnection()
     console.log("Connected to the database!")
@@ -28,11 +35,6 @@ async function testConnection() {
 }
 
 testConnection()
-
-// export async function getUsers() {
-//   const [rows] = await pool.query("SELECT * FROM users")
-//   return rows
-// }
 
 export async function getUsers() {
   try {
