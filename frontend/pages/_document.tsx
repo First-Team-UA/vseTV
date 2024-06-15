167339d242f6f33da6d9951b429876b96e1c9556
svitlana-horodylova <horodylova.sv@gmail.com>
2024-06-15 12:24:37 +0100

    Update _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,17 +1,29 @@
+// pages/_document.js (или _document.tsx для TypeScript)
+
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="TV channels database" />
+
+        
+          <link rel="stylesheet" href="/frontend/styles/globals.css" />
+
+      
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
           <NextScript />
+
         </body>
       </Html>
     );
