901da02293b3ac8567c2bb20296a7af0762be74a
CheekyGeroin <dan.97198@gmail.com>
2024-06-24 22:07:19 +0200

    add new func for auth route
    Add new func for auth route. Fix auth middleware

--- backend/src/controllers/auth/auth.ts
+++ backend/src/controllers/auth/auth.ts
@@ -1,19 +1,25 @@
-// import HttpError from '../../helpers/HttpError';
-// import ctrlWrapper from '../../helpers/ctrlWrapper';
+import HttpError from '../../helpers/HttpError';
+import JWTHandling from '../../helpers/JWTHandling';
+import ctrlWrapper from '../../helpers/ctrlWrapper';
 //import hashing from '../../helpers/hashing';
-import { getAuth } from '../../services/database/authQueries';
+import { ClientAuth, getAuth, logout, setToken } from '../../services/database/authQueries';
 import { NextFunction, Request, Response } from 'express';
 
-export const logIn = async (
+ const logIn = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   const { login, password } = req.body;
-  const testResult = await getAuth(login, password);
-  console.log(testResult);
+  const testResult = await getAuth(login, password) as ClientAuth;
 
-  res.send(testResult);
+   const newToken = await JWTHandling.signToken(testResult.id.toString())
+
+   await setToken(newToken, testResult.id)
+   
+   const finalResult = await getAuth(login,password) as ClientAuth
+
+  res.send(finalResult);
   ``;
 };
 
@@ -21,4 +27,19 @@ export const logOut = async (
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  const { id } = req.params
+  try {
+    await logout(Number(id))
+    res.send('LogoutSuccessful')
+  } catch (error) {
+    const err = new HttpError(404, "Not Found")
+    throw err
+  }
+};
+
+
+export const authCtrl = {
+  logIn: ctrlWrapper(logIn),
+  logOut: ctrlWrapper(logOut)
+}
