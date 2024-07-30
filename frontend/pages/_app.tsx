8f1d1969bb7743de9020d8a7203f89c4613ccbcd
CheekyGeroin <dan.97198@gmail.com>
2024-07-30 17:52:42 +0200

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,23 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import GlobalStyles from '../styles/GlobalStyles';
 import '../i18n';
+import SpriteLoader from '../сomponents/SpriteLoader/SpriteLoader';
 import store, { persistor } from '@frontend/redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />;
-      </PersistGate>
-    </Provider>
+    <>
+      <GlobalStyles />
+      <SpriteLoader />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />;
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 
